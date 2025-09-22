0de32d92026abbb6ab074df7f4a2a8d271ac4cf6
Peritract <dan.keefe@sigmalabs.co.uk>
2025-09-22 12:40:27 +0100

    Add article table

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,13 @@
 CREATE TABLE outlet (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    name TEXT UNIQUE
+    outlet_id INT GENERATED ALWAYS AS IDENTITY,
+    name TEXT UNIQUE,
+    PRIMARY KEY (article_id)
+);
+
+CREATE TABLE article (
+    article_id INT GENERATED ALWAYS AS IDENTITY,
+    title TEXT NOT NULL,
+    outlet_id INT NOT NULL,
+    FOREIGN KEY outlet_id REFERENCES outlet(outlet_id)
+    PRIMARY KEY article_id
 );
